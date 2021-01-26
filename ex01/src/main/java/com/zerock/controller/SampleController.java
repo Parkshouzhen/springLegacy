f34c452b4b71b050ea72d1789c3a8030f642ddab
Parkshouzhen <shouzhenn@gmail.com>
2021-01-26 17:21:03 +0900

    spring mvc 설정

---
+++ ex01/src/main/java/com/zerock/controller/SampleController.java
@@ -0,0 +1,126 @@
+package com.zerock.controller;
+
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/sample/*")
+@Log4j
+public class SampleController {
+
+	@RequestMapping("")
+	public void basic() {
+
+		log.info("basic.......");
+	}
+
+	@GetMapping("/basicOnlyGet")
+	public void basicGet1() {
+		log.info("basic get only get.........");
+	}
+
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+		log.info(dto);
+		return "ex01";
+	}
+
+	@GetMapping("/ex02")
+	public String ex01(@RequestParam("name") String name, @RequestParam("age") int age) {
+		log.info("name: " + name);
+		log.info("age: " + age);
+
+		return "ex02";
+	}
+
+	@GetMapping("/ex02List")
+	public String ex02List(@RequestParam("ids") ArrayList<String> ids) {
+		log.info("ids: " + ids);
+		return "ex02List"; // 스프링은 파라미터의 타입을 보고 객체를 생성하므로 파라미터의 타입은 List<>와 같이 인테페이스 타입이 아닌 실제적인 클래스 타입으로
+							// 지정함
+	}
+
+	@GetMapping("/ex02Array")
+	public String ex02List(@RequestParam("ids") String[] ids) {
+		log.info("ids arr: " + Arrays.toString(ids));
+		return "ex02Array"; //
+	}
+
+	@GetMapping("/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+		log.info("list dtos: " + list);
+		return "ex02Bean";
+	}
+
+	/*
+	 * @InitBinder // DTO에서 @DateTimeFormat(pattern = "yyyy/MM/dd") 해주면 initBinder
+	 * 필요 없음 public void initBinder(WebDataBinder binder) { SimpleDateFormat
+	 * dataFormat = new SimpleDateFormat("yyyy-MM-dd");
+	 * binder.registerCustomEditor(java.util.Date.class, new
+	 * CustomDateEditor(dataFormat, false)); }
+	 */
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo: " + todo);
+		return "ex03";
+	}
+
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) { // @ModelAttribute : 강제로 전달받은 파라미터를 Model에 담아서
+																			// 전달하도록 할 때 필요
+		log.info("dtp: " + dto);
+		log.info("page: " + page);
+
+		return "/sample/ex04";
+	}
+
+	@GetMapping("/ex05")
+	public void ex05() {
+		log.info("/ex05............");
+
+	}
+
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		log.info("/ex06............");
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(40);
+		dto.setName("박소분");
+		
+		return dto;
+	}
+
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07() {
+		log.info("/ex07............");
+		
+		String msg = "{\"name\":\"박소분\"}";
+		
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-Type", "application/json;charset=UTF-8");
+		
+		return new ResponseEntity<String>(msg, header, HttpStatus.OK);
+		
+	}
+}
